f044e2b82ec81c813a9d7df433cc49ccfef96018
B Varun Gupta <84367762+bvarungupta@users.noreply.github.com>
2024-01-01 00:19:06 +0530

    Add files via upload

---
+++ src/context/store.ts
@@ -0,0 +1,31 @@
+import React from "react";
+
+
+export type TodoType = {
+    todoId : string,
+    title  : string,
+    status : string,
+    startDate : string,
+    DeadLine  : string
+}
+
+export type ProjectType = {
+    title : string
+    projectId : string,
+    todos : TodoType[] ,
+}
+
+
+type StoreType  = {
+       projects : ProjectType[],
+       addProject : (project:ProjectType)=>void,
+       currentProjectId : string,
+       setCurrentProject : (projectId :string) => void,
+       SetTodoToCurrentProject : (Todo : TodoType) => void,
+       getTodosForCurrentProject : (currentProjectId:string)=> TodoType[]
+}
+
+
+const store = React.createContext<StoreType>({} as StoreType);
+
+export default store;
